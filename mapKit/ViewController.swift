e4429aaabfe4b531122133f6eecae838933219bd
Developer <Dev@02s-imac.es>
2018-03-28 16:46:19 +0400

    Feature 2

--- mapKit/ViewController.swift
+++ mapKit/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         //Feature 1
+        //Feature 2
         
         // Do any additional setup after loading the view, typically from a nib.
     }
